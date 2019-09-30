b0ec437d2ce4e6eee70d8f1043912dd386c05e5b
George Witteman <george@witteman.me>
2019-09-29 20:55:58 -0700

    Move stuff into separate files

---
+++ zsh-prompt-gitstatus.zsh
@@ -0,0 +1,58 @@
+function gitstatus_prompt_update() {
+  emulate -L zsh
+  typeset -g  GITSTATUS_PROMPT=''
+  typeset -gi GITSTATUS_PROMPT_LEN=0
+
+  # Call gitstatus_query synchronously. Note that gitstatus_query can also be called
+  # asynchronously; see documentation in gitstatus.plugin.zsh.
+  gitstatus_query 'MY'                  || return 1  # error
+  [[ $VCS_STATUS_RESULT == 'ok-sync' ]] || return 0  # not a git repo
+
+  local clean='%F{magenta}'   # green foreground
+  local modified='%F{yellow}'  # yellow foreground
+  local untracked='%F{blue}'   # blue foreground
+  local conflicted='%F{red}'  # red foreground
+
+  local before
+  local after
+
+  local where  # branch name, tag or commit
+  if [[ -n $VCS_STATUS_LOCAL_BRANCH ]]; then
+    where=$VCS_STATUS_LOCAL_BRANCH
+  elif [[ -n $VCS_STATUS_TAG ]]; then
+    before+='%f#'
+    where=$VCS_STATUS_TAG
+  else
+    before+='%f@'
+    where=${VCS_STATUS_COMMIT[1,8]}
+  fi
+
+  (( $#where > 32 )) && where[13,-13]="…"  # truncate long branch names and tags
+  before+="${clean}${where//\%/%%}"             # escape %
+
+  # ⇣42 if behind the remote.
+  (( VCS_STATUS_COMMITS_BEHIND )) && before+="%f↓${VCS_STATUS_COMMITS_BEHIND}"
+  # ⇡42 if ahead of the remote; no leading space if also behind the remote: ⇣42⇡42.
+  (( VCS_STATUS_COMMITS_AHEAD && !VCS_STATUS_COMMITS_BEHIND )) && before+=""
+  (( VCS_STATUS_COMMITS_AHEAD  )) && before+="%f↑${VCS_STATUS_COMMITS_AHEAD}"
+
+  # 'merge' if the repo is in an unusual state.
+  [[ -n $VCS_STATUS_ACTION     ]] && after+="${conflicted}${VCS_STATUS_ACTION}"
+  # ~42 if have merge conflicts.
+  (( VCS_STATUS_NUM_CONFLICTED )) && after+="%F{magenta}✖${VCS_STATUS_NUM_CONFLICTED}"
+  # +42 if have staged changes.
+  (( VCS_STATUS_NUM_STAGED     )) && after+="%F{green}✚${VCS_STATUS_NUM_STAGED}"
+  # !42 if have unstaged changes.
+  (( VCS_STATUS_NUM_UNSTAGED   )) && after+="%F{red}✚${VCS_STATUS_NUM_UNSTAGED}"
+  # ?42 if have untracked files. It's really a question mark, your font isn't broken.
+  (( VCS_STATUS_NUM_UNTRACKED  )) && after+="%F{blue}…${VCS_STATUS_NUM_UNTRACKED}"
+  # *42 if have stashes.
+  (( VCS_STATUS_STASHES        )) && after+="%F{yellow}*${VCS_STATUS_STASHES}"
+
+  local sep
+  [ "$after" != "" ] && sep='%f|'
+  GITSTATUS_PROMPT=" (${before}${sep}${after}%f)"
+
+  # The length of GITSTATUS_PROMPT after removing %f and %F.
+  GITSTATUS_PROMPT_LEN="${(m)#${${GITSTATUS_PROMPT//\%\%/x}//\%(f|<->F)}}"
+}
--- zsh-prompt.plugin.zsh
+++ zsh-prompt.plugin.zsh
@@ -1,68 +1,15 @@
 setopt prompt_subst
 autoload -Uz add-zsh-hook
-
 add-zsh-hook precmd my_prompt_precmd
+
+MY_PROMPT_DIR="${0:a:h}"
+
+source "$MY_PROMPT_DIR/zsh-prompt-nice-exit-code.zsh"
+source "$MY_PROMPT_DIR/zsh-prompt-gitstatus.zsh"
 
 gitstatus_stop 'MY' && gitstatus_start -s -1 -u -1 -c -1 -d -1 'MY'
 
-function gitstatus_prompt_update() {
-  emulate -L zsh
-  typeset -g  GITSTATUS_PROMPT=''
-  typeset -gi GITSTATUS_PROMPT_LEN=0
-
-  # Call gitstatus_query synchronously. Note that gitstatus_query can also be called
-  # asynchronously; see documentation in gitstatus.plugin.zsh.
-  gitstatus_query 'MY'                  || return 1  # error
-  [[ $VCS_STATUS_RESULT == 'ok-sync' ]] || return 0  # not a git repo
-
-  local clean='%F{magenta}'   # green foreground
-  local modified='%F{yellow}'  # yellow foreground
-  local untracked='%F{blue}'   # blue foreground
-  local conflicted='%F{red}'  # red foreground
-
-  local before
-  local after
-
-  local where  # branch name, tag or commit
-  if [[ -n $VCS_STATUS_LOCAL_BRANCH ]]; then
-    where=$VCS_STATUS_LOCAL_BRANCH
-  elif [[ -n $VCS_STATUS_TAG ]]; then
-    before+='%f#'
-    where=$VCS_STATUS_TAG
-  else
-    before+='%f@'
-    where=${VCS_STATUS_COMMIT[1,8]}
-  fi
-
-  (( $#where > 32 )) && where[13,-13]="…"  # truncate long branch names and tags
-  before+="${clean}${where//\%/%%}"             # escape %
-
-  # ⇣42 if behind the remote.
-  (( VCS_STATUS_COMMITS_BEHIND )) && before+="%f↓${VCS_STATUS_COMMITS_BEHIND}"
-  # ⇡42 if ahead of the remote; no leading space if also behind the remote: ⇣42⇡42.
-  (( VCS_STATUS_COMMITS_AHEAD && !VCS_STATUS_COMMITS_BEHIND )) && before+=""
-  (( VCS_STATUS_COMMITS_AHEAD  )) && before+="%f↑${VCS_STATUS_COMMITS_AHEAD}"
-
-  # 'merge' if the repo is in an unusual state.
-  [[ -n $VCS_STATUS_ACTION     ]] && after+="${conflicted}${VCS_STATUS_ACTION}"
-  # ~42 if have merge conflicts.
-  (( VCS_STATUS_NUM_CONFLICTED )) && after+="%F{magenta}✖${VCS_STATUS_NUM_CONFLICTED}"
-  # +42 if have staged changes.
-  (( VCS_STATUS_NUM_STAGED     )) && after+="%F{green}✚${VCS_STATUS_NUM_STAGED}"
-  # !42 if have unstaged changes.
-  (( VCS_STATUS_NUM_UNSTAGED   )) && after+="%F{red}✚${VCS_STATUS_NUM_UNSTAGED}"
-  # ?42 if have untracked files. It's really a question mark, your font isn't broken.
-  (( VCS_STATUS_NUM_UNTRACKED  )) && after+="%F{blue}…${VCS_STATUS_NUM_UNTRACKED}"
-  # *42 if have stashes.
-  (( VCS_STATUS_STASHES        )) && after+="%F{yellow}*${VCS_STATUS_STASHES}"
-
-  local sep
-  [ "$after" != "" ] && sep='%f|'
-  GITSTATUS_PROMPT=" (${before}${sep}${after}%f)"
-
-  # The length of GITSTATUS_PROMPT after removing %f and %F.
-  GITSTATUS_PROMPT_LEN="${(m)#${${GITSTATUS_PROMPT//\%\%/x}//\%(f|<->F)}}"
-}
+type should_drink >/dev/null 2>&1 && HYDRATE_INSTALLED=1
 
 jobscount() {
   JOBSCOUNT="%(1j. %F{15}%K{yellow} %j %f%k.)"
@@ -70,7 +17,9 @@ jobscount() {
 
 my_prompt_precmd() {
   RETVAL="$?"
+  typeset -g PROMPT_DRINK=''
   VCS_INFO=""
+  [ "$HYDRATE_INSTALLED" != "0" ] && PROMPT_DRINK="$(drink_water)"
   gitstatus_prompt_update
   jobscount
   my_prompt_render
@@ -78,15 +27,20 @@ my_prompt_precmd() {
 
 my_prompt_render() {
   PVENV=
-  result=
+  exit_code=
   if [ "$RETVAL" != "0" ] && [ -n "$RETVAL" ]; then
-    result=" %K{red}%F{15} $RETVAL %k%f"
+    nice_code=$(nice_exit_code "$RETVAL")
+    if [ "$nice_code" != "" ]; then
+      exit_code=" %K{red}%F{15} $nice_code ($RETVAL) %k%f"
+    else
+      exit_code=" %K{red}%F{15} $RETVAL %k%f"
+    fi
   fi
   if [ "$VIRTUAL_ENV" != "" ]; then
     PVENV="$(basename "$VIRTUAL_ENV") "
   fi
-  PROMPT='%F{244}${PVENV}%f%F{cyan}%B%~%f%b$GITSTATUS_PROMPT %% '
-  RPROMPT='$JOBSCOUNT$result'
+  PROMPT='%F{244}${PVENV}%f%F{cyan}%B%~%f%b$GITSTATUS_PROMPT $PROMPT_DRINK%% '
+  RPROMPT='$JOBSCOUNT$exit_code'
 }
 
 my_prompt_render
