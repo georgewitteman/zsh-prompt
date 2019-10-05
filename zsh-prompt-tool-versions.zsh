9c94e97259e192bb6a607960ba409306559facce
George Witteman <george@witteman.me>
2019-10-04 23:35:35 -0700

    Clean up

--- zsh-prompt-tool-versions.zsh
+++ zsh-prompt-tool-versions.zsh
@@ -2,17 +2,18 @@ get_tool_version_if_not_default() {
   local name=$1
   local file_name=$2
   local found_dir=$PWD
-  while [ "${found_dir##/${HOME[2,-1]}/}" != "${found_dir}" ]; do
-    [ -f "${found_dir}/${file_name}" ] || continue
-    while IFS=' ' read -r tool version extra_versions; do
-      # Skip over lines containing comments.
-      # (Lines starting with '#').
-      [ "${tool##\#*}" ] || continue
-      if [ "$tool" = "$name" ] && [ "$version" != "" ]; then
-        REPLY="$version"
-        return
-      fi
-    done < "${found_dir}/${file_name}"
+  while [ ${#HOME} -lt ${#found_dir} ]; do
+    if [ -f "${found_dir}/${file_name}" ]; then
+      while IFS=' ' read -r tool version extra_versions; do
+        # Skip over lines containing comments.
+        # (Lines starting with '#').
+        [ "${tool##\#*}" ] || continue
+        if [ "$tool" = "$name" ] && [ "$version" != "" ]; then
+          REPLY="$version"
+          return
+        fi
+      done < "${found_dir}/${file_name}"
+    fi
     found_dir=${found_dir:a:h}
   done
   unset REPLY
