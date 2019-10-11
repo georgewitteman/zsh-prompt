5c04a5ebd2e548106cdecfec28eefe6ca32db3ff
George Witteman <george@witteman.me>
2019-10-11 13:22:08 -0700

    Remove a bunch of stuff

--- zsh-prompt.plugin.zsh
+++ zsh-prompt.plugin.zsh
@@ -1,30 +1,38 @@
 setopt prompt_subst
 
-source "${0:a:h}/zsh-prompt-shrink-path.zsh"
-source "${0:a:h}/zsh-prompt-tool-versions.zsh"
-source "${0:a:h}/zsh-prompt-git-head.zsh"
-
 VIRTUAL_ENV_DISABLE_PROMPT=1
-PS_DIR=1
-PS_GIT_HEAD=2
-PS_NODE_VERSION=3
-PS_PYTHON_VERSION=4
+PS_GIT_HEAD=1
 
-chpwd() {
-  shrink_path $PS_DIR
+prompt_git_head() {
+  psvar[$PS_GIT_HEAD]=''
+
+  local git_root=$PWD
+  # Search up each directory until we get to the root or find one
+  # that has a git repository
+  until [ -d "${git_root}/.git" ] || [ "$git_root" = "/" ]; do
+    git_root=${git_root:a:h}
+  done
+
+  # Check if we found a git repo
+  [ -f "${git_root}/.git/HEAD" ] || return 1
+
+  # Read contents of HEAD file
+  local head=$(<"${git_root}/.git/HEAD")
+  if [[ $head == 'ref: '* ]]; then
+    psvar[$PS_GIT_HEAD]=${head##ref: refs\/heads\/}
+  else
+    psvar[$PS_GIT_HEAD]=${head:0:10}
+  fi
 }
-chpwd # Set up the first short prompt
 
 precmd() {
-  prompt_git_head $PS_GIT_HEAD
-  get_node_version $PS_NODE_VERSION
-  get_python_version $PS_PYTHON_VERSION
+  prompt_git_head
 }
 
 echo "${fg_bold[blue]}Don't forget to drink water!${reset_color}"
 
 ## Left prompt
-PS1=""
+PS1=''
 # SSH
 PS1+='${${_FP_IS_SSH::="${SSH_TTY}${SSH_CONNECTION}${SSH_CLIENT}"}+}'
 PS1+='${_FP_IS_SSH:+"%F{15}%K{cyan} SSH %k%f "}'
@@ -33,10 +41,10 @@ PS1+='${_FP_IS_SSH:+"%F{15}%K{cyan} SSH %k%f "}'
 PS1+='${VIRTUAL_ENV:+"%F{242}${VIRTUAL_ENV:t}%f "}'
 
 # Short path if available
-PS1+="%F{cyan}%(${PS_DIR}V.%${PS_DIR}v.%~) %f"
+PS1+="%B%F{cyan}%1~ %f%b"
 
 # Git HEAD
-PS1+="%(${PS_GIT_HEAD}V.(%F{magenta}%B%${PS_GIT_HEAD}v%f%b) .)"
+PS1+="%(${PS_GIT_HEAD}V.(%F{magenta}%${PS_GIT_HEAD}v%f) .)"
 
 # Background jobs
 PS1+="%(1j.%F{yellow}%j:bg%f .)"
@@ -51,10 +59,5 @@ PS1+="%(0?..%F{red})%#%f "
 PS2='%F{242}%_… %f>%f '
 
 ## Right prompt
-RPS1=''
 # Exit code
-RPS1+='%(0?.. %K{red}%F{15} ${signals[$status-127]:-$status} %k%f)'
-
-# Tool versions
-RPS1+="%(${PS_NODE_VERSION}V.%F{green} nodejs:%${PS_NODE_VERSION}v.)"
-RPS1+="%(${PS_PYTHON_VERSION}V.%F{yellow} python:%${PS_PYTHON_VERSION}v.)"
+RPS1='%(0?.. %K{red}%F{15} ${signals[$status-127]:-$status} %k%f)'
