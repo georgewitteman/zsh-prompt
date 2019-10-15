ff1caa7377d689bbfd05992fce2b76b536842684
George Witteman <george@witteman.me>
2019-10-15 09:34:56 -0700

    Use PROMPT and RPROMPT instead of PS1 and RPS1

--- zsh-prompt.plugin.zsh
+++ zsh-prompt.plugin.zsh
@@ -29,34 +29,32 @@ precmd() {
   prompt_git_head
 }
 
-echo "${fg_bold[blue]}Don't forget to drink water!${reset_color}"
-
 ## Left prompt
-PS1=''
+PROMPT=''
 # SSH
-PS1+='${${_FP_IS_SSH::="${SSH_TTY}${SSH_CONNECTION}${SSH_CLIENT}"}+}'
-PS1+='${_FP_IS_SSH:+"%F{15}%K{cyan} SSH %k%f "}'
+PROMPT+='${${_FP_IS_SSH::="${SSH_TTY}${SSH_CONNECTION}${SSH_CLIENT}"}+}'
+PROMPT+='${_FP_IS_SSH:+"%F{15}%K{cyan} SSH %k%f "}'
 
 # Virtual env
-PS1+='${VIRTUAL_ENV:+"%F{242}${VIRTUAL_ENV:t}%f "}'
+PROMPT+='${VIRTUAL_ENV:+"%F{242}${VIRTUAL_ENV:t}%f "}'
 
 # Short path if available
-PS1+="%B%F{cyan}%1~ %f%b"
+PROMPT+="%B%F{cyan}%1~ %f%b"
 
 # Git HEAD
-# PS1+="%(${PS_GIT_HEAD}V.(%F{magenta}%${PS_GIT_HEAD}v%f) .)"
+# PROMPT+="%(${PS_GIT_HEAD}V.(%F{magenta}%${PS_GIT_HEAD}v%f) .)"
 
 # Background jobs
-PS1+="%(1j.%F{yellow}%j:bg%f .)"
+PROMPT+="%(1j.%F{yellow}%j:bg%f .)"
 
 # Subshell warning
-PS1+="%(3L.%F{yellow}%L+%f .)"
+PROMPT+="%(3L.%F{yellow}%L+%f .)"
 
 # Prompt character
-PS1+="%(0?..%F{red})%#%f "
+PROMPT+="%(0?..%F{red})%#%f "
 
 ## Continuation prompt
-PS2='%F{242}%_… %f>%f '
+PROMPT2='%F{242}%_… %f>%f '
 
 ## Right prompt
 # Don't add the random extra space at the end of the right prompt
@@ -66,7 +64,7 @@ PS2='%F{242}%_… %f>%f '
 # ZLE_RPROMPT_INDENT=0
 
 # Exit code
-RPS1='%(0?.. %K{red}%F{15} ${signals[$status-127]:-$status} %k%f)'
+RPROMPT='%(0?.. %K{red}%F{15} ${signals[$status-127]:-$status} %k%f)'
 
 # Git HEAD
-RPS1+="%(${PS_GIT_HEAD}V. git:%F{magenta}%${PS_GIT_HEAD}v%f.)"
+RPROMPT+="%(${PS_GIT_HEAD}V. git:%F{magenta}%${PS_GIT_HEAD}v%f.)"
