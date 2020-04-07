33c4862312fe45168bcc3c09c713cc2c8deb093a
George Witteman <george@witteman.me>
2020-04-07 11:42:03 -0700

    Add native grep solution to test script

--- test/grep-comparison.zsh
+++ test/grep-comparison.zsh
@@ -1,4 +1,5 @@
-TEST_COUNT=750
+# TEST_COUNT=750
+TEST_COUNT=250
 TESTFILE="${0:a:h}/testfile.txt"
 echo -n 'cat $TESTFILE | grep searchstring: '
 _TIMER=$EPOCHREALTIME
@@ -24,6 +25,17 @@ echo -n 'fgrep searchstring $TESTFILE: '
 _TIMER=$EPOCHREALTIME
 for ((i = 0; i < $TEST_COUNT; i++)); do
   MYVAR=$(fgrep searchstring $TESTFILE)
+done
+now=$(($EPOCHREALTIME*1000))
+_TIMER=$(($_TIMER*1000))
+startup_diff=$((($now - $_TIMER) / $TEST_COUNT))
+echo "${startup_diff[0,7]}ms"
+
+echo -n 'native zsh: '
+_TIMER=$EPOCHREALTIME
+for ((i = 0; i < $TEST_COUNT; i++)); do
+  lines=( "${(@f)"$(<$TESTFILE)"}" )
+  MYVAR=( ${(M)lines:#*searchstring*} )
 done
 now=$(($EPOCHREALTIME*1000))
 _TIMER=$(($_TIMER*1000))
