a77ab35badb4a16a1cec40796860d49467fbfbf2
George Witteman <george.witteman@affirm.com>
2020-09-13 21:53:14 -0400

    Add git HEAD and venv

--- zsh-prompt.plugin.zsh
+++ zsh-prompt.plugin.zsh
@@ -1,5 +1,70 @@
+setopt prompt_subst
+
+prompt-precmd() {
+  GIT_PROMPT=
+
+  # Search up each directory until we get to the root or find one
+  # that has a git repository
+  local git_root="$PWD"
+  until [[ "$git_root" = "/" || -d "${git_root}/.git" ]]; do
+    git_root="${git_root:a:h}"
+  done
+
+  # Check if we found a git repo
+  [[ "$git_root" != "/" && -f "${git_root}/.git/HEAD" ]] || return
+
+  # Read contents of HEAD file
+  local head=$(<"${git_root}/.git/HEAD")
+  if [[ "$head" == 'ref: '* ]]; then
+    head=${head##ref: refs\/heads\/}
+  else
+    head=${head:0:10}
+  fi
+
+  GIT_PROMPT="%F{magenta}${head}%f"
+
+  # Branching state
+  if [[ -f "$git_root/.git/rebase-merge/interactive" ]]; then
+    GIT_PROMPT+=":%F{yellow}rebase-i%f"
+  elif [[ -d "$git_root/.git/rebase-merge" ]]; then
+    GIT_PROMPT+=":%F{yellow}rebase-m%f"
+  elif [[ -d "$git_root/.git/rebase-apply" ]]; then
+    if [[ -f "$git_root/rebase-apply/rebasing" ]]; then
+      GIT_PROMPT+=":%F{yellow}rebase%f"
+    elif [[ -f "$git_root/.git/rebase-apply/applying" ]]; then
+      GIT_PROMPT+=":%F{yellow}am%f"
+    else
+      GIT_PROMPT+=":%F{yellow}am/r%f"
+    fi
+  elif [[ -f "$git_root/.git/MERGE_HEAD" ]]; then
+    GIT_PROMPT+=":%F{yellow}merge%f"
+  elif [[ -f "$git_root/.git/BISECT_LOG" ]]; then
+    GIT_PROMPT+=":%F{yellow}bisect%f"
+  fi
+
+  # Set # of stashes
+  [[ -f "${git_root}/.git/logs/refs/stash" ]] || return
+
+  local stashes=("${(f)$(<${git_root}/.git/logs/refs/stash)}")
+  [[ "${#stashes}" -eq 0 ]] && return
+  GIT_PROMPT+=" [%F{yellow}${#stashes}%f "
+  if [[ "${#stashes}" -eq 1 ]]; then
+    GIT_PROMPT+="stash]"
+  else
+    GIT_PROMPT+="stashes]"
+  fi
+}
+
+[[ -z "${precmd_functions+1}" ]] && precmd_functions=()
+if [[ ${precmd_functions[(ie)prompt-precmd]} -gt ${#precmd_functions} ]]; then
+  precmd_functions+=(prompt-precmd)
+fi
+
 ## Left prompt
 PS1=
+
+VIRTUAL_ENV_DISABLE_PROMPT=1
+PS1+='${VIRTUAL_ENV:+%F{242\}${VIRTUAL_ENV:t}%f }'
 
 # Short path if available
 PS1+="%F{cyan}%~ %f%b"
@@ -34,6 +99,9 @@ RPS1=
 
 # Exit code
 RPS1+='%(0?.. %K{red}%F{15} %? %k%f)'
+
+# Git info
+RPS1+='${GIT_PROMPT:+ }${GIT_PROMPT}'
 
 # Time
 RPS1+=" %D{%L:%M %p}"
